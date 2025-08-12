9e48092d14733cd7a4be5284df3aa385d2095adb
Rahaf alzahrani <s230044976@seu.edu.sa>
2025-08-12 17:11:48 +0300

    Create Array with catch.java

---
+++ Array with catch.java
@@ -0,0 +1,27 @@
+
+
+public class Array
+{
+    public static void main(String [] args){
+         
+        int Mynumbers[]  = {14, 3, 21, 98, 01, 96};
+        
+        for( int numbers : Mynumbers)
+        
+        System.out.println(numbers);
+        
+        try {
+            int myNumbers[] = {1, 2 ,3};
+            
+            System.out.print(myNumbers[10]);
+        }
+        catch(Exception e) { 
+            System.out.print(" it does not work");
+        
+    }
+    
+    
+}
+
+}
+
